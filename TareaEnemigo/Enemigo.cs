1e026314d2450af48b4cc59e0943b5abc3f4cdb8
Andres Sanchez Gomez <42181445+ElCazaVampiros@users.noreply.github.com>
2018-08-15 21:26:36 -0700

    Add files via upload

---
+++ TareaEnemigo/Enemigo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pokemon pokemon1 = new Pokemon();
+            pokemon1.positionX = 2;
+            pokemon1.positionY = 4;
+            pokemon1.movimiento = 4;
+            pokemon1.ataque = 8;
+            if (pokemon1.ataque < 5)
+            {
+                pokemon1.puntos = 1000;
+            }
+            else if (pokemon1.ataque < 2)
+            {
+                pokemon1.ataque = 100;
+            }
+
+            Console.WriteLine("Pokemon salvaje a aparecido");
+            Console.WriteLine(" ");
+            Console.WriteLine("Eevee:");
+            Console.WriteLine("posición X:" + pokemon1.positionX);
+            Console.WriteLine("posición Y: " + pokemon1.positionY);
+            Console.WriteLine("Numero de movimientos: " + pokemon1.movimiento);
+            Console.WriteLine("Ataques: " + pokemon1.ataque);
+            Console.WriteLine(" ");
+            if (pokemon1.puntos == 1000 || pokemon1.puntos < 1000)
+            {
+                Console.WriteLine("Has derrotado a Eevee");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TareaEnemigo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Pokemon
+    {
+        public int positionX { get; set; }
+        public int positionY { get; set; }
+        public int explosion { get; set; }
+        public int movimiento { get; set; }
+        public int puntos { get; set; }
+        private int _ataque;
+        public int ataque
+        {
+            get
+            {
+                return _ataque;
+            }
+            set
+            {
+                if (value >= 4)
+                {
+                    _ataque = value;
+
+                }
+                else if (value < 4)
+                {
+                    Console.WriteLine("+500PUNTOS");
+                    Console.WriteLine("Points: " + puntos + 500);
+                }
+                else if (value == 0) ;
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+    }
+}
